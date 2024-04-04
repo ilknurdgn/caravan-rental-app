8ab5c1d3df18c0840e775d19dd76f710c40223e6
Semanur Özkan <semanurozkan4444@gmail.com>
2024-04-04 21:53:21 +0300

    feat: create initial navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Navbar from './componnets/Navbar/Navbar';
+import ExampleComponent from './componnets/exampleComponent/ExampleComponent';
+
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div className='main'>
+     <Navbar/>
+     
+    </div>
   );
 }
 
