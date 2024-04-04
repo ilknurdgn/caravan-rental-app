605873b01a09eb8422ea9cf00e624297bb494eaa
ilknur doğan <ilknurdogan254@gmail.com>
2024-04-04 23:37:28 +0300

    feat: Connect MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-app.listen(5000, () => {
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => console.log(err));
+
+app.listen('8000', () => {
   console.log('Backend is running.');
 });
