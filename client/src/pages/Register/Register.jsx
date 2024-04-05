9736efed037a06a7003f18da070c55e14296a883
isinnur <isinngnyy@gmail.com>
2024-04-05 12:06:51 +0300

    feat: add register container

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,7 +1,43 @@
 import styles from "./register.module.css";
-
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className={styles.register}>
+      <div className={styles.registerContainer}>
+        <span className={styles.registerTitle}>REGISTER</span>
+        <form className={styles.form}>
+          <input
+            type="text"
+            placeholder="First name"
+            className={styles.registerInput}
+          />
+          <input
+            type="password"
+            placeholder="Last name"
+            className={styles.registerInput}
+          />
+          <input
+            type="date"
+            placeholder="birthday"
+            className={styles.registerInput}
+          />
+          <input
+            type="email"
+            placeholder="hello@hello.com"
+            className={styles.registerInput}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className={styles.registerInput}
+          />
+
+          <button className={styles.registerButton} type="submit">
+            Continue
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
