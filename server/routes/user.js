a3aa68fdda2c40461000b9717210c8958440d311
İlknur Doğan <ilknurdogan254@gmail.com>
2024-04-07 01:42:14 +0300

    feat(routes): create 'get user' function

--- server/routes/user.js
+++ server/routes/user.js
@@ -49,4 +49,16 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+//GET USER
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
