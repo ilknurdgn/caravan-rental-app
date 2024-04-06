ad03c59e7b0de194c10b128fe390e49cb2f584a4
İlknur Doğan <ilknurdogan254@gmail.com>
2024-04-07 01:27:33 +0300

    feat(routes): create 'delete user' funciton

--- server/routes/user.js
+++ server/routes/user.js
@@ -30,4 +30,23 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+//DELETE
+router.delete('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json('User has been deleted.');
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } catch (error) {
+      res.status(404).json('User not found');
+    }
+  } else {
+    res.status(401).json('You can delete only account!');
+  }
+});
+
 module.exports = router;
