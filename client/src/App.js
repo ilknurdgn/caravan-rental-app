6001124077bd3da2d63f0054d41ee69d31c54c55
isinnur <isinngnyy@gmail.com>
2024-04-06 15:27:07 +0300

    style: change selection color

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,32 @@ import Home from './pages/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Register from './pages/Register/Register';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useSearchParams,
+} from 'react-router-dom';
 import Login from './pages/Login/Login';
 import HowToRent from './pages/HowToRent/HowToRent';
+import { useState } from 'react';
 
 function App() {
-  const [user, setUser] = useState(false);
   return (
     <div className='main'>
-      <Login />
-      {/* <Navbar />
-      <Footer /> */}
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/about' element={<Home />} />
+          <Route path='/how-to-rent' element={<HowToRent />} />
+          <Route path='/caravans' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+
+      <Footer />
     </div>
   );
 }
