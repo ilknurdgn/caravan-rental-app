983a41b87adcfc0911c28d0624739c7a69cc0cea
isinnur <isinngnyy@gmail.com>
2024-04-06 15:11:32 +0300

    feat(Register): translate into turkish

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import Login from "./pages/Login/Login";
-import { useState } from "react";
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Login from './pages/Login/Login';
+import { useState } from 'react';
+import Register from './pages/Register/Register';
 
 function App() {
   const [user, setUser] = useState(false);
   return (
-    <div className="main">
+    <div className='main'>
       {user && <Navbar />}
-      <Login />
+      <Register />
       {user && <Footer />}
     </div>
   );
