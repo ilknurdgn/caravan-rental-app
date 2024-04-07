0a63433b9ab38f9f8cb012d01afdf5436769bb63
isinnur <isinngnyy@gmail.com>
2024-04-07 14:53:06 +0300

    feat(register): make register system functional

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,8 +1,33 @@
+import { useState } from 'react';
 import styles from './register.module.css';
+import axios from 'axios';
 const Register = () => {
-  const handleSubmit = (e) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    setError(false);
+    try {
+      const res = await axios.post('/auth/register', {
+        firstName,
+        lastName,
+        birthday,
+        email,
+        password,
+      });
+      console.log(res);
+      res.data && window.location.replace('/login');
+    } catch (err) {
+      setError(true);
+      console.log(err);
+    }
   };
+
   return (
     <div className={styles.register}>
       <div className={styles.registerContainer}>
@@ -12,28 +37,37 @@ const Register = () => {
             type='text'
             placeholder='Ad'
             className={styles.registerInput}
+            onChange={(e) => setFirstName(e.target.value)}
           />
           <input
-            type='password'
+            type='text'
             placeholder='Soyad'
             className={styles.registerInput}
+            onChange={(e) => setLastName(e.target.value)}
           />
-          <input type='date' className={styles.registerInput} />
+          <input
+            type='date'
+            className={styles.registerInput}
+            onChange={(e) => setBirthday(e.target.value)}
+          />
           <input
             type='email'
             placeholder='test@test.com'
             className={styles.registerInput}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type='password'
             placeholder='Şifre'
             className={styles.registerInput}
+            onChange={(e) => setPassword(e.target.value)}
           />
 
           <button className={styles.registerButton} type='submit'>
             Devam et
           </button>
         </form>
+        {error && <span>Hatalı ya da eksik bilgi girildi.</span>}
       </div>
     </div>
   );
