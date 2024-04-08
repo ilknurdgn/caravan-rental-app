9354156583d03da6c37d2cedc78b57e535d6b563
isinnur <isinngnyy@gmail.com>
2024-04-08 18:24:01 +0300

    feat(Context): add user to localstorage

--- client/src/context/Contex.js
+++ client/src/context/Contex.js
@@ -1,7 +1,7 @@
 import { Children, createContext, useEffect, useReducer } from 'react';
 import Reducer from './Reducer';
 const INITIAL_STATE = {
-  user: null,
+  user: JSON.parse(localStorage.getItem('user')) || null,
   isFetching: false,
   error: false,
 };
@@ -10,6 +10,11 @@ export const Context = createContext(INITIAL_STATE);
 
 export const ContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
+
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(state.user));
+  }, [state.user]);
+
   return (
     <Context.Provider
       value={{
