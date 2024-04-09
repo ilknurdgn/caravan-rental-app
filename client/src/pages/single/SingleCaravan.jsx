cff9fad228b5e848328ca3beba02f1772684106a
isinnur <isinngnyy@gmail.com>
2024-04-09 13:49:43 +0300

    feat(singleCaravan): add caravan description

--- client/src/pages/single/SingleCaravan.jsx
+++ client/src/pages/single/SingleCaravan.jsx
@@ -1,12 +1,26 @@
+import { CiShare2 } from 'react-icons/ci';
+import { FaRegHeart } from 'react-icons/fa6';
+import { RiCaravanLine } from 'react-icons/ri';
+import { PiGasPump } from 'react-icons/pi';
+import { IoSettingsOutline } from 'react-icons/io5';
+import { GoPeople } from 'react-icons/go';
+import { IoMdStar } from 'react-icons/io';
 import styles from './singleCaravan.module.css';
+
 const SingleCaravan = () => {
   return (
     <div className={styles['single-container']}>
       <div className={styles['caravan-info']}>
-        <span className={styles['caravan-title']}>Motocaravan-Antalya</span>
+        <span className={styles['caravan-title']}>Motocaravan - Antalya</span>
         <div className={styles.icons}>
-          <div className={styles.share}>share</div>
-          <div className={styles.fav}>ekle</div>
+          <div className={styles['right-side']}>
+            <CiShare2 className={styles.icon} />
+            <p>Paylaş</p>
+          </div>
+          <div className={styles['right-side']}>
+            <FaRegHeart className={styles.icon} />
+            <p>Ekle</p>
+          </div>
         </div>
       </div>
 
@@ -38,6 +52,66 @@ const SingleCaravan = () => {
             alt=''
           />
         </div>
+      </div>
+
+      <div className={styles['description-section']}>
+        <div className={styles['caravan-left-side']}>
+          <span>2023 Yapımı 4+ Kişilik, Kiralık Motokaravan</span>
+          <ul className={styles.features}>
+            <li>
+              <RiCaravanLine /> <p>Motokaravan</p>
+            </li>
+            <li>
+              <PiGasPump /> <p>Dizel</p>
+            </li>
+            <li>
+              <IoSettingsOutline /> <p>Manuel</p>
+            </li>
+            <li>
+              <GoPeople /> <p> 4 Kişilik</p>
+            </li>
+          </ul>
+
+          <ul className={styles.review}>
+            <li>
+              <IoMdStar className={styles['star-icon']} /> 4,83 · 1,800
+              değerlendirme
+            </li>
+          </ul>
+
+          <div className={styles.profile}>
+            <div className={styles['profile-pic']}>
+              <img
+                className={styles['owner-profile-pic']}
+                src='https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                alt=''
+              />
+            </div>
+            <div className={styles.owner}> Karavan Sahibi: Işınnur Günay</div>
+          </div>
+
+          <div className={styles.line}></div>
+
+          <div className={styles.description}>
+            <span>AÇIKLAMA</span>
+            <p>
+              Antalya, Döşemealtı bölgesinde bulunan Kiralık Motokaravan; <br />
+              <ul>
+                <li className={styles.mn}>2016 modeldir.</li>
+                <li> 2023 yılında karavan yapılmıştır.</li>
+                <li>
+                  Dizel yakıt kullanılır ve Manuel vites özelliği bulunur.
+                </li>
+                <li>Karavanımız toplam 4 kişiliktir.</li>
+                <li>Sigara kullanımına uygun değildir.</li>
+                <li>Evcil hayvan kabul edilmemektedir.</li>
+                <li>Kiralık karavanımızın kiralama kaskosu bulunmaktadır.</li>
+                <li> Karavanımız B tipi ehliyet ile kullanılabilmektedir.</li>
+              </ul>
+            </p>
+          </div>
+        </div>
+        <div className={styles['caravan-right-side']}>300tl</div>
       </div>
     </div>
   );
