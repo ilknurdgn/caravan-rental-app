c499f5bf2c4dbb7014e9da23e903b3fa34d4f83b
isinnur <isinngnyy@gmail.com>
2024-04-29 00:02:03 +0300

    feat(Favorites): add new photos

--- client/src/pages/favorites/Favorites.jsx
+++ client/src/pages/favorites/Favorites.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
-
+import styles from './favorites.module.css';
+import Pagination from '@mui/material/Pagination';
+import { FaRegHeart } from 'react-icons/fa';
+import FavoriteCaravan from '../../components/favoriteCaravan/FavoriteCaravan';
 const Favorites = () => {
-  return <div>Favorites</div>;
+  return (
+    <div className={styles['caravans-container']}>
+      <span className={styles.name}>Favoriler</span>
+      <div className={styles.caravans}>
+        <FavoriteCaravan />
+        <FavoriteCaravan />
+        <FavoriteCaravan />
+        <FavoriteCaravan />
+        <FavoriteCaravan />
+        <FavoriteCaravan />
+        <FavoriteCaravan />
+        <FavoriteCaravan />
+        <FavoriteCaravan />
+        <FavoriteCaravan />
+        <FavoriteCaravan />
+      </div>
+      <Pagination
+        className={styles.pagination}
+        count={10}
+        color='primary'
+        variant='outlined'
+        size='large'
+      />
+    </div>
+  );
 };
 
 export default Favorites;
