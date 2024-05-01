cad8c05961a83c0562baf0c66f5d295454372ede
isinnur <isinngnyy@gmail.com>
2024-05-01 19:13:00 +0300

    feat(SingleBlog): add update blog

--- client/src/pages/SingleBlog/SingleBlog.jsx
+++ client/src/pages/SingleBlog/SingleBlog.jsx
@@ -6,11 +6,30 @@ import { BsTrash3 } from 'react-icons/bs';
 import { CiEdit } from 'react-icons/ci';
 const SingleBlog = () => {
   const [updateMode, setUpdateMode] = useState(false);
+  const [title, setTitle] = useState('');
+  const [blog, setBlog] = useState('');
+
+  const update = () => {
+    setUpdateMode(true);
+    setTitle(title);
+  };
+
   return (
     <div className={styles['single-blog']}>
-      <span className={styles.title}>Neden VANCA?</span>
+      {updateMode ? (
+        <input
+          type='text'
+          value={title}
+          className={styles.singlePostTitleInput}
+          onChange={(e) => setTitle(e.target.value)}
+          autoFocus
+        />
+      ) : (
+        <span className={styles.title}>Neden VANCA?</span>
+      )}
+
       <div className={styles.update}>
-        <span className={styles.edit}>
+        <span onClick={update} className={styles.edit}>
           <CiEdit />
         </span>
         <span className={styles.delete}>
@@ -36,47 +55,54 @@ const SingleBlog = () => {
         </div>
 
         <div className={styles['blog-content']}>
-          <span className={styles.subtitle}>
-            Neden LUX’dan karavan kiralamalısınız?
-          </span>
-          <p className={styles['blog-entry']}>
-            Karavan seyahati, özgürlük, macera ve keyif dolu anlarla dolu bir
-            deneyim sunar. Ancak, seyahat deneyiminizi unutulmaz kılmak için
-            doğru ekipmana sahip olmanız önemlidir. İşte bu noktada, LUX Karavan
-            Kiralama hizmeti devreye giriyor ve size unutulmaz bir karavan
-            macerası sunuyor. Peki, neden LUX'dan karavan kiralamanız
-            gerektiğini merak ediyor musunuz? İşte cevabı:
-            <br /> Kalite ve Konfor <br /> LUX, kaliteli ve konforlu karavanlar
-            sunar. Her detay özenle düşünülerek tasarlanmıştır ve
-            karavanlarımızda ihtiyacınız olan her şeyi bulabilirsiniz. Rahat
-            yataklar, mutfak ekipmanları, temiz banyo olanakları ve daha
-            fazlasıyla donatılmış karavanlarımızla konforlu bir seyahat deneyimi
-            yaşarsınız. <br /> Çeşitlilik ve Seçenekler <br /> LUX, farklı
-            ihtiyaçlara ve tercihlere hitap eden geniş bir karavan filosuna
-            sahiptir. Tek kişilik gezilerden aile tatillerine kadar her türlü
-            seyahat için uygun karavan seçenekleri mevcuttur. Size en uygun
-            olanı seçme özgürlüğüne sahipsiniz. <br /> Güvenlik ve Bakım <br />{' '}
-            LUX, müşterilerinin güvenliğini ve memnuniyetini ön planda tutar.
-            Karavanlarımız düzenli olarak bakım ve temizlikten geçer, böylece
-            güvenli ve sağlıklı bir seyahat deneyimi yaşamanızı sağlarız.
-            Ayrıca, herhangi bir sorunla karşılaştığınızda 7/24 müşteri desteği
-            sunarız. <br /> Esneklik ve Özgürlük <br /> LUX karavan kiralama
-            hizmeti, size seyahat planlarınızda esneklik ve özgürlük sunar.
-            Kendi hızınızda ve istediğiniz rotada seyahat edebilir, dilediğiniz
-            zaman durup dinlenebilir veya yeni yerler keşfedebilirsiniz.
-            Karavanınızı istediğiniz gibi kişiselleştirebilir ve kendi eviniz
-            gibi hissedebilirsiniz. <br /> Uygun Fiyatlar
-            <br /> LUX, kaliteli bir karavan deneyimi sunarken uygun fiyatlarla
-            hizmet verir. Karavan kiralama ücretleri, seyahat süresine ve tercih
-            ettiğiniz karavan modeline göre değişebilir, ancak genel olarak
-            uygun ve rekabetçi fiyatlar sunarız. Sonuç olarak, LUX'dan karavan
-            kiralamanız size unutulmaz bir seyahat deneyimi sunar. Kaliteli ve
-            konforlu karavanlarımız, geniş seçeneklerimiz, güvenliğimiz ve uygun
-            fiyatlarımızla size mükemmel bir karavan macerası yaşatmak için
-            buradayız. Şimdi, hayalinizdeki karavan tatiline bir adım daha
-            yakınsınız. LUX ile bugün rezervasyon yapın ve unutulmaz bir
-            maceraya adım atın!
-          </p>
+          {updateMode ? (
+            <textarea type='text' className={styles.singlePostDescInput} />
+          ) : (
+            <div>
+              <span className={styles.subtitle}>
+                Neden LUX’dan karavan kiralamalısınız?
+              </span>
+              <p className={styles['blog-entry']}>
+                Karavan seyahati, özgürlük, macera ve keyif dolu anlarla dolu
+                bir deneyim sunar. Ancak, seyahat deneyiminizi unutulmaz kılmak
+                için doğru ekipmana sahip olmanız önemlidir. İşte bu noktada,
+                LUX Karavan Kiralama hizmeti devreye giriyor ve size unutulmaz
+                bir karavan macerası sunuyor. Peki, neden LUX'dan karavan
+                kiralamanız gerektiğini merak ediyor musunuz? İşte cevabı:
+                <br /> Kalite ve Konfor <br /> LUX, kaliteli ve konforlu
+                karavanlar sunar. Her detay özenle düşünülerek tasarlanmıştır ve
+                karavanlarımızda ihtiyacınız olan her şeyi bulabilirsiniz. Rahat
+                yataklar, mutfak ekipmanları, temiz banyo olanakları ve daha
+                fazlasıyla donatılmış karavanlarımızla konforlu bir seyahat
+                deneyimi yaşarsınız. <br /> Çeşitlilik ve Seçenekler <br /> LUX,
+                farklı ihtiyaçlara ve tercihlere hitap eden geniş bir karavan
+                filosuna sahiptir. Tek kişilik gezilerden aile tatillerine kadar
+                her türlü seyahat için uygun karavan seçenekleri mevcuttur. Size
+                en uygun olanı seçme özgürlüğüne sahipsiniz. <br /> Güvenlik ve
+                Bakım <br /> LUX, müşterilerinin güvenliğini ve memnuniyetini ön
+                planda tutar. Karavanlarımız düzenli olarak bakım ve temizlikten
+                geçer, böylece güvenli ve sağlıklı bir seyahat deneyimi
+                yaşamanızı sağlarız. Ayrıca, herhangi bir sorunla
+                karşılaştığınızda 7/24 müşteri desteği sunarız. <br /> Esneklik
+                ve Özgürlük <br /> LUX karavan kiralama hizmeti, size seyahat
+                planlarınızda esneklik ve özgürlük sunar. Kendi hızınızda ve
+                istediğiniz rotada seyahat edebilir, dilediğiniz zaman durup
+                dinlenebilir veya yeni yerler keşfedebilirsiniz. Karavanınızı
+                istediğiniz gibi kişiselleştirebilir ve kendi eviniz gibi
+                hissedebilirsiniz. <br /> Uygun Fiyatlar
+                <br /> LUX, kaliteli bir karavan deneyimi sunarken uygun
+                fiyatlarla hizmet verir. Karavan kiralama ücretleri, seyahat
+                süresine ve tercih ettiğiniz karavan modeline göre değişebilir,
+                ancak genel olarak uygun ve rekabetçi fiyatlar sunarız. Sonuç
+                olarak, LUX'dan karavan kiralamanız size unutulmaz bir seyahat
+                deneyimi sunar. Kaliteli ve konforlu karavanlarımız, geniş
+                seçeneklerimiz, güvenliğimiz ve uygun fiyatlarımızla size
+                mükemmel bir karavan macerası yaşatmak için buradayız. Şimdi,
+                hayalinizdeki karavan tatiline bir adım daha yakınsınız. LUX ile
+                bugün rezervasyon yapın ve unutulmaz bir maceraya adım atın!
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
