c1fd37557c9686c95c636e44dee1cfe55c2872b8
isinnur <isinngnyy@gmail.com>
2024-05-02 12:03:57 +0300

    feat(Write): add submit function

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -1,7 +1,39 @@
 import styles from './write.module.css';
+import { RiImageAddLine } from 'react-icons/ri';
 
 const Write = () => {
-  return <div className={styles.write}>Write</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.write}>
+      <form onSubmit={handleSubmit} className={styles.writeForm}>
+        <div className={styles.writeFormGroup}>
+          <input
+            type='text'
+            className={styles.writeInput}
+            autoFocus={true}
+            placeholder='Başlık ekle'
+          />
+          <label className={styles.fileInput} htmlFor='fileInput'>
+            <RiImageAddLine /> Fotoğraf ekle
+          </label>
+          <input type='file' id='fileInput' style={{ display: 'none' }} />
+        </div>
+        <div className={styles.writeFormGroup}>
+          <textarea
+            placeholder='Hikayeni yaz...'
+            type='text'
+            className={styles.writeText}
+          ></textarea>
+        </div>
+        <button className={styles.writeSubmit} type='submit'>
+          Yayınla
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Write;
