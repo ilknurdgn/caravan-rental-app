a1f61ae240d0c960aabbd13ee13151f9b7c70613
İlknur Doğan <ilknurdogan254@gmail.com>
2024-05-11 12:38:08 +0300

    feat(favoriteCaravansController): create 'delete' function

--- server/routes/favoriteCaravansRoute.js
+++ server/routes/favoriteCaravansRoute.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const favoriteCaravanController = require('../controllers/favoriteCaravanController');
 
 router.post('/add', favoriteCaravanController.add);
+router.delete('/delete', favoriteCaravanController.delete);
 
 module.exports = router;
