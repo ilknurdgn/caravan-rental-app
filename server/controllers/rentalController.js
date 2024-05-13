8ec6954c9dc6c1e0b8e186745aa5f3f9fd2a5b46
İlknur Doğan <ilknurdogan254@gmail.com>
2024-05-13 22:11:04 +0300

    feat: create getBookings function

--- server/controllers/rentalController.js
+++ server/controllers/rentalController.js
@@ -2,6 +2,7 @@ const Rental = require('../models/rentalModel');
 const User = require('../models/userModel');
 const Caravan = require('../models/caravanModel');
 
+// ADD BOOKING
 exports.booking = async (req, res) => {
   try {
     const userId = req.body.userId;
@@ -30,6 +31,34 @@ exports.booking = async (req, res) => {
     const rental = await newRental.save();
 
     res.status(200).json(rental);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// GET ALL BOOKINGS
+exports.getBookings = async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found!' });
+    }
+
+    const rentals = await Rental.find({ userId: userId });
+
+    for (let rental of rentals) {
+      const currentDate = new Date();
+
+      if (currentDate > rental.endDate && rental.status !== 'completed') {
+        rental.status = 'completed';
+
+        await rental.save();
+      }
+    }
+
+    res.status(200).json(rentals);
   } catch (error) {
     res.status(500).json(error);
   }
--- server/routes/rentalRoute.js
+++ server/routes/rentalRoute.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const rentalController = require('../controllers/rentalController');
 
 router.post('/booking', rentalController.booking);
+router.get('/getBookings', rentalController.getBookings);
 
 module.exports = router;
