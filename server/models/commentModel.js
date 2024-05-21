1cf5c5d2c77af3c8cc2a997e97b15e10d636569c
İlknur Doğan <ilknurdogan254@gmail.com>
2024-05-21 16:00:39 +0300

    feat(models): create comment model

---
+++ server/models/commentModel.js
@@ -0,0 +1,24 @@
+const { default: mongoose } = require('mongoose');
+const mongose = require('mongoose');
+
+const commentSchema = new mongoose.Schema(
+  {
+    caravan: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Caravan',
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Comment', commentSchema);
