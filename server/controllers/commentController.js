6c4f22af8e2db52d8f5e0f63104fa2fddda460de
İlknur Doğan <ilknurdogan254@gmail.com>
2024-05-24 15:26:56 +0300

    feat: create comment update feature

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -13,12 +13,8 @@ exports.add = async (req, res) => {
       res.status(404).json({ message: 'Caravan not found!' });
     }
 
-    const userId = req.body.userId;
-    const user = await User.findById(userId);
-
-    if (!user) {
-      res.status(404).json({ message: 'User not found!' });
-    }
+    const objectId = req.user._id;
+    const userId = objectId.toString();
 
     const rental = await Rental.findOne({
       userId: userId,
@@ -27,7 +23,7 @@ exports.add = async (req, res) => {
     });
 
     if (!rental) {
-      res.status(403).json({
+      return res.status(403).json({
         message: 'You can only comment on rentals you have completed',
       });
     }
@@ -43,5 +39,38 @@ exports.add = async (req, res) => {
     res.status(200).json({ message: 'Comment saved', comment: comment });
   } catch (error) {
     res.status(500).json({ message: 'Comment could not be saved', error });
+  }
+};
+
+// UPDATE COMMENT
+exports.update = async (req, res) => {
+  try {
+    const commentId = req.params.id;
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+      return res.status(403).json({ message: 'Comment not found!' });
+    }
+
+    const objectId = req.user._id;
+    const userId = objectId.toString();
+
+    if (!comment.user.equals(userId)) {
+      return res
+        .status(403)
+        .json({ message: 'You are not authorized to update this comment' });
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(
+      commentId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    res.status(200).json({ message: 'Comment updated.', updatedComment });
+  } catch (error) {
+    res.status(500).json({ message: 'Comment could not be updated', error });
   }
 };
--- server/routes/commentRoute.js
+++ server/routes/commentRoute.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const commentController = require('../controllers/commentController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post('/add', commentController.add);
+router.post('/add', authMiddleware, commentController.add);
+router.put('/update/:id', authMiddleware, commentController.update);
 
 module.exports = router;
