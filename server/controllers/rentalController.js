cfc82d25949c345be85078696c67de36370e9199
İlknur Doğan <ilknurdogan254@gmail.com>
2024-05-27 14:24:00 +0300

    refactor: Remove unnecessary user ID checks

--- server/controllers/rentalController.js
+++ server/controllers/rentalController.js
@@ -5,12 +5,7 @@ const Caravan = require('../models/caravanModel');
 // ADD BOOKING
 exports.booking = async (req, res) => {
   try {
-    const userId = req.body.userId;
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(404).json({ message: 'User not found!' });
-    }
+    const userId = req.user._id.toString();
 
     const caravanId = req.body.caravanId;
     const caravan = await Caravan.findById(caravanId);
@@ -74,12 +69,7 @@ exports.getSingleBooking = async (req, res) => {
 // GET ALL BOOKINGS
 exports.getBookings = async (req, res) => {
   try {
-    const userId = req.body.userId;
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(404).json({ message: 'User not found!' });
-    }
+    const userId = req.user._id.toString();
 
     const rentals = await Rental.find({ userId: userId });
 
@@ -138,8 +128,8 @@ exports.cancelBooking = async (req, res) => {
         const index = caravan.notAvailableDates.indexOf(notAvailableDate);
         if (index > -1) {
           caravan.notAvailableDates.splice(index, 1);
+          caravan.save();
         }
-        caravan.save();
       }
     });
 
--- server/routes/rentalRoute.js
+++ server/routes/rentalRoute.js
@@ -3,8 +3,12 @@ const rentalController = require('../controllers/rentalController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/booking', authMiddleware, rentalController.booking);
-router.get('/getSingleBooking/:id', rentalController.getSingleBooking);
-router.get('/getBookings', rentalController.getBookings);
+router.get(
+  '/getSingleBooking/:id',
+  authMiddleware,
+  rentalController.getSingleBooking
+);
+router.get('/getBookings', authMiddleware, rentalController.getBookings);
 router.put(
   '/cancelBooking/:id',
   authMiddleware,
