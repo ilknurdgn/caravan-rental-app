423ea08c1cbde8a1b0510ff5b02c6f965a0c64cf
Semanur Özkan <semanurozkan4444@gmail.com>
2024-05-27 12:49:40 +0300

    fix(personalInformation): update functions name & add new states

--- client/src/pages/PersonalInformation/PersonalInformation.jsx
+++ client/src/pages/PersonalInformation/PersonalInformation.jsx
@@ -2,6 +2,14 @@ import React, { useState } from 'react';
 import styles from './personalInformation.module.css';
 import { differenceInDays } from 'date-fns';
 
+const user = {
+  _id: '1',
+  firstName: 'ufuk can',
+  lastName: 'kurt',
+  email: 'ufuk',
+  password: '1234',
+};
+
 const PersonalInformation = () => {
   const [isActiveName, setIsActiveName] = useState(false);
   const [isActiveMail, setIsActiveMail] = useState(false);
@@ -10,8 +18,19 @@ const PersonalInformation = () => {
 
   const [name, setName] = useState('');
   const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [newPasswordAgain, setNewPasswordAgain] = useState('');
 
-  const handleChangeName = (e) => {
+  console.log('name:', name);
+  console.log('surname:', surname);
+  console.log('email:', email);
+  console.log('phone:', phone);
+  console.log('newPassword:', newPassword);
+  console.log('newPasswordAgain:', newPasswordAgain);
+
+  const handleVisibleName = (e) => {
     if (isActiveName === false) {
       setIsActiveName(true);
     } else {
@@ -19,7 +38,7 @@ const PersonalInformation = () => {
     }
   };
 
-  const handleChangeMail = (e) => {
+  const handleVisibleMail = (e) => {
     if (isActiveMail === false) {
       setIsActiveMail(true);
     } else {
@@ -27,7 +46,7 @@ const PersonalInformation = () => {
     }
   };
 
-  const handleChangePhone = (e) => {
+  const handleVisiblePhone = (e) => {
     if (isActivePhone === false) {
       setIsActivePhone(true);
     } else {
@@ -35,16 +54,13 @@ const PersonalInformation = () => {
     }
   };
 
-  const handleChangePassword = (e) => {
+  const handleVisiblePassword = (e) => {
     if (isActivePassword === false) {
       setIsActivePassword(true);
     } else {
       setIsActivePassword(false);
     }
   };
-
-  console.log('name:', name);
-  console.log('surnamename:', surname);
 
   return (
     <div className={styles.container}>
@@ -58,7 +74,7 @@ const PersonalInformation = () => {
         <div className={styles.infoContainer}>
           <div className={styles.info}>
             <span>Ad Soyad</span>
-            <button className={styles.button} onClick={handleChangeName}>
+            <button className={styles.button} onClick={handleVisibleName}>
               {isActiveName ? `İptal Et` : `Düzenle`}
             </button>
           </div>
@@ -96,7 +112,7 @@ const PersonalInformation = () => {
         <div className={styles.infoContainer}>
           <div className={styles.info}>
             <span>E-posta adresi</span>
-            <button className={styles.button} onClick={handleChangeMail}>
+            <button className={styles.button} onClick={handleVisibleMail}>
               {isActiveMail ? `İptal Et` : `Düzenle`}
             </button>
           </div>
@@ -108,7 +124,11 @@ const PersonalInformation = () => {
               <div className={styles.inputContainer}>
                 <div className={styles.inputContent}>
                   <div className={styles.ad}>E-posta Adresi</div>
-                  <input type='text' />
+                  <input
+                    type='text'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
               </div>
 
@@ -122,7 +142,7 @@ const PersonalInformation = () => {
         <div className={styles.infoContainer}>
           <div className={styles.info}>
             <span>Telefon Numarası</span>
-            <button className={styles.button} onClick={handleChangePhone}>
+            <button className={styles.button} onClick={handleVisiblePhone}>
               {isActivePhone ? `İptal Et` : `Düzenle`}
             </button>
           </div>
@@ -132,7 +152,11 @@ const PersonalInformation = () => {
               <div className={styles.inputContainer}>
                 <div className={styles.inputContent}>
                   <div className={styles.ad}>Telefon Numarası</div>
-                  <input type='text' />
+                  <input
+                    type='text'
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
                 </div>
               </div>
 
@@ -146,7 +170,7 @@ const PersonalInformation = () => {
         <div className={styles.infoContainer}>
           <div className={styles.info}>
             <span>Parola</span>
-            <button className={styles.button} onClick={handleChangePassword}>
+            <button className={styles.button} onClick={handleVisiblePassword}>
               {isActivePassword ? `İptal Et` : `Güncelle`}
             </button>
           </div>
@@ -154,24 +178,36 @@ const PersonalInformation = () => {
             <div>
               <div className={styles.content}>Güçlü bir parola oluşturun.</div>
               <div className={styles.passwordInputContainer}>
-                <div className={styles.passwordInput}>
+                {/* <div className={styles.passwordInput}>
                   <div>Mevcut Parola</div>
                   <div className={styles.inputContent}>
-                    <input type='text' />
+                    <input
+                      type='text'
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
                   </div>
-                </div>
+                </div> */}
 
                 <div>
                   <div>Yeni Parola</div>
                   <div className={styles.inputContent}>
-                    <input type='text' />
+                    <input
+                      type='text'
+                      value={newPassword}
+                      onChange={(e) => setNewPassword(e.target.value)}
+                    />
                   </div>
                 </div>
 
                 <div>
                   <div>Parolayı Onaylayın</div>
                   <div className={styles.inputContent}>
-                    <input type='text' />
+                    <input
+                      type='text'
+                      value={newPasswordAgain}
+                      onChange={(e) => setNewPasswordAgain(e.target.value)}
+                    />
                   </div>
                 </div>
               </div>
