f98138e04c3c81508268c5753f7c1ec031accdd4
İlknur Doğan <ilknurdogan254@gmail.com>
2024-05-29 12:39:58 +0300

    feat: enable photos uploaded with Multer to be saved in MinIO

--- server/controllers/caravanController.js
+++ server/controllers/caravanController.js
@@ -1,25 +1,62 @@
 const Caravan = require('../models/caravanModel');
+const minioClient = require('../minioClient');
+const multer = require('multer');
 
 //Add a caravan in the DB
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage }).array('photos', 5);
+
 exports.add = async (req, res) => {
   try {
-    const newCaravan = new Caravan({
-      photos: req.body.photos,
-      title: req.body.title,
-      location: req.body.location,
-      type: req.body.type,
-      fuel: req.body.fuel,
-      gear: req.body.gear,
-      maxGuests: req.body.maxGuests,
-      dailyPrice: req.body.dailyPrice,
-      owner: req.body.owner,
-      yearOfManufacture: req.body.yearOfManufacture,
-      description: req.body.description,
-      notAvailableDates: req.body.notAvailableDates,
-    });
+    upload(req, res, async function (err) {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
 
-    const caravan = await newCaravan.save();
-    res.status(200).json(caravan);
+      const uploadedPhotos = [];
+      for (const file of req.files) {
+        const metaData = {
+          'Content-Type': file.mimetype,
+        };
+
+        await minioClient.fPutObject(
+          'vanca',
+          `caravans/${file.filename}`,
+          file.path,
+          metaData
+        );
+
+        uploadedPhotos.push(
+          `http://89.252.131.245:9000/vanca/caravans/${file.filename}`
+        );
+      }
+
+      const newCaravan = new Caravan({
+        photos: uploadedPhotos,
+        title: req.body.title,
+        location: req.body.location,
+        type: req.body.type,
+        fuel: req.body.fuel,
+        gear: req.body.gear,
+        maxGuests: req.body.maxGuests,
+        dailyPrice: req.body.dailyPrice,
+        owner: req.body.owner,
+        yearOfManufacture: req.body.yearOfManufacture,
+        description: req.body.description,
+        notAvailableDates: req.body.notAvailableDates,
+      });
+
+      const caravan = await newCaravan.save();
+      res.status(200).json(caravan);
+    });
   } catch (error) {
     res.status(500).json(error);
   }
@@ -46,14 +83,29 @@ exports.updateCaravan = async (req, res) => {
 exports.deleteCaravan = async (req, res) => {
   try {
     const caravan = await Caravan.findById(req.params.id);
-    try {
-      await Caravan.findByIdAndDelete(req.params.id);
-      res.status(200).json('Caravan has been deleted.');
-    } catch (error) {
-      res.status(500).json(error);
+    if (!caravan) {
+      return res.status(404).json('Caravan not found');
     }
+
+    const photos = caravan.photos;
+    if (photos.length !== 0) {
+      for (const photo of photos) {
+        try {
+          const photoKey = photo.replace(
+            'http://89.252.131.245:9000/vanca/',
+            ''
+          );
+          await minioClient.removeObject('vanca', photoKey);
+        } catch (error) {
+          console.error('Error removing photo from MinIO:', error);
+        }
+      }
+    }
+
+    await Caravan.findByIdAndDelete(req.params.id);
+    res.status(200).json('Caravan has been deleted.');
   } catch (error) {
-    res.status(404).json('User not found');
+    res.status(500).json(error);
   }
 };
 
