20e26ebc5f3ab3bc50a58b9b3583efb2aaee8216
isinnur <isinngnyy@gmail.com>
2024-05-29 22:39:59 +0300

    feat: add favorite caravans photos

--- client/src/components/favoriteCaravan/FavoriteCaravan.jsx
+++ client/src/components/favoriteCaravan/FavoriteCaravan.jsx
@@ -5,10 +5,7 @@ const FavoriteCaravan = ({ caravan }) => {
   return (
     <div className={styles.caravan}>
       <div className={styles['caravan-image']}>
-        <img
-          src='https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-          alt=''
-        />
+        <img src={caravan.photos[0]} alt='' />
       </div>
       <div className={styles['caravans-info']}>
         <div className={styles['caravans-content']}>
